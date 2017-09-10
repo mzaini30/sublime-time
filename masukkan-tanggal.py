a9b6a6e9aead9cfa11b86ecf620a7f044df31765
mzaini30 <muhzaini30@gmail.com>
2017-09-10 15:18:25 +0800

    oke

--- masukkan-tanggal.py
+++ masukkan-tanggal.py
@@ -24,6 +24,9 @@ class InsertDatetimeCommand(sublime_plugin.TextCommand):
         waktu = re.sub(r"Oct", r"Oktober", waktu);
         waktu = re.sub(r"Nov", r"November", waktu);
         waktu = re.sub(r"Des", r"Desember", waktu);
-        
+
+        waktu = waktu.split();
+        waktu = ""+waktu[0]+", "+waktu[2]+" "+waktu[1]+" "+waktu[4]+" pukul "+waktu[3]+"";
+
         for s in sel:
             self.view.replace(edit, s, waktu)
